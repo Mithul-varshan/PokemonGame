3056dcc4511dbd2d0bf5e41cada5890869308bf5
Mithul-varshan <mithul0605@gmail.com>
2026-02-11 21:57:32 +0530

    implemented player movement

--- index.js
+++ index.js
@@ -1,20 +1,58 @@
-const canvas = document.querySelector('canvas');
+const canvas = document.querySelector("canvas");
 
-const c = canvas.getContext('2d')
+const c = canvas.getContext("2d");
 
-canvas.width  = 1024
-canvas.height = 576
-c.fillStyle = 'white'
-c.fillRect(0, 0, canvas.width, canvas.height)
+canvas.width = 1024;
+canvas.height = 576;
 
-const image  = new Image()
-image.src = 'img/Pellet Town.png'
-const playerImage = new Image()
-playerImage.src = './img/playerDown.png'
+c.fillStyle = "white";
+c.fillRect(0, 0, canvas.width, canvas.height);
+
+const image = new Image();
+image.src = "img/Pellet Town.png";
+
+const playerImage = new Image();
+playerImage.src = "./img/playerDown.png";
+
+
+class Sprite {
+    constructor({position, velocity, image}) {
+        this.position = position
+        this.image = image
+    }
+
+    draw() {
+        c.drawImage(this.image, this.position.x, this.position.y)
+    }
+}
+
+const background = new Sprite({
+    position: {
+        x: -738,
+        y: -605
+    },
+    image: image
+})
+
+const keys = {
+    w: {
+        pressed: false
+    },
+    a: {
+        pressed: false
+    },
+    s: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+}
 
 //tile image adjusting
-image.onload = () => {
-    c.drawImage(image, -700, -550)
+function animate(){
+    window.requestAnimationFrame(animate)
+    background.draw()
     c.drawImage(
         playerImage,
         0,
@@ -24,31 +62,54 @@ image.onload = () => {
         canvas.width / 2 - playerImage.width / 4 / 2,
         canvas.height / 2 - playerImage.height / 2,
         playerImage.width / 4,
-        playerImage.height
-    )
-}
+        playerImage.height,
+    );
 
-function animate() {
-    window.requestAnimationFrame()
-    console.log('animate')
-}
+    if(keys.w.pressed && lastKey === 'w') background.position.y += 3
+    else if(keys.a.pressed && lastKey === 'a') background.position.x += 3
+    else if(keys.d.pressed && lastKey === 'd') background.position.x -= 3
+    else if(keys.s.pressed && lastKey === 's') background.position.y -= 3
 
+}
 animate()
 
-window.addEventListener('keydown', (e) => {
-    switch (e.key) {
-        case 'w':
-            console.log('pressed w key')
-            break
-        case 'a':
-            console.log('pressed a key')
-            break
-        case 's':
-            console.log('pressed s key')
-            break
-        case 'd':
-            console.log('pressed w key')
-            break
-    }
-})
+let lastKey = ''
 
+window.addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case "w":
+        keys.w.pressed = true
+        lastKey = 'w'
+        break;
+    case "a":
+        keys.a.pressed = true
+        lastKey = 'a'
+        break;
+    case "s":
+        keys.s.pressed = true
+        lastKey = 's'
+        break;
+    case "d":
+        keys.d.pressed = true
+        lastKey = 'd'
+        break;
+  }
+  console.log(keys)
+});
+window.addEventListener("keyup", (e) => {
+  switch (e.key) {
+    case "w":
+        keys.w.pressed = false
+        break;
+    case "a":
+        keys.a.pressed = false
+        break;
+    case "s":
+        keys.s.pressed = false
+        break;
+    case "d":
+        keys.d.pressed = false
+        break;
+  }
+  console.log(keys)
+});
