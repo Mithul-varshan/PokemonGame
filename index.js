ea76c9d392dac496fdca068bb0aebc9738b17ee0
Mithul-varshan <mithul0605@gmail.com>
2026-02-14 11:15:46 +0530

    started implementing boundaries

--- index.js
+++ index.js
@@ -5,6 +5,47 @@ const c = canvas.getContext("2d");
 canvas.width = 1024;
 canvas.height = 576;
 
+const collisionsMap = [];
+for (let i = 0; i < collisions.length; i += 70) {
+  collisionsMap.push(collisions.slice(i, 70 + i));
+}
+
+class Boundary {
+  static width = 48;
+  static height = 48;
+  constructor({ position }) {
+    this.position = position;
+    this.width = 48;
+    this.height = 48;
+  }
+
+  draw() {
+    c.fillStyle = "red";
+    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+}
+
+const boundaries = [];
+
+const offset = {
+  x: -735,
+  y: -650,
+};
+
+collisionsMap.forEach((row, i) => {
+  row.forEach((symbol, j) => {
+    if (symbol === 1025)
+      boundaries.push(
+        new Boundary({
+          position: {
+            x: j * Boundary.width + offset.x,
+            y: i * Boundary.height + offset.y,
+          },
+        }),
+      );
+  });
+});
+
 c.fillStyle = "white";
 c.fillRect(0, 0, canvas.width, canvas.height);
 
@@ -14,102 +55,150 @@ image.src = "img/Pellet Town.png";
 const playerImage = new Image();
 playerImage.src = "./img/playerDown.png";
 
-
 class Sprite {
-    constructor({position, velocity, image}) {
-        this.position = position
-        this.image = image
-    }
+  constructor({ position, velocity, image, frames = { max: 1 } }) {
+    this.position = position;
+    this.image = image;
+    this.frames = frames;
 
-    draw() {
-        c.drawImage(this.image, this.position.x, this.position.y)
-    }
+    this.image.onload = () => {
+      this.width = this.image.width / this.frames.max;
+      this.height = this.image.height;
+    };
+  }
+
+  draw() {
+    c.drawImage(
+      this.image,
+      0,
+      0,
+      this.image.width / this.frames.max,
+      this.image.height,
+      this.position.x,
+      this.position.y,
+      this.image.width / this.frames.max,
+      this.image.height,
+    );
+  }
 }
+
+const player = new Sprite({
+  position: {
+    x: canvas.width / 2 - 192 / 4 / 2,
+    y: canvas.height / 2 - 68 / 2,
+  },
+  image: playerImage,
+  frames: {
+    max: 4,
+  },
+});
 
 const background = new Sprite({
-    position: {
-        x: -738,
-        y: -605
-    },
-    image: image
-})
+  position: {
+    x: offset.x,
+    y: offset.y,
+  },
+  image: image,
+});
 
 const keys = {
-    w: {
-        pressed: false
-    },
-    a: {
-        pressed: false
-    },
-    s: {
-        pressed: false
-    },
-    d: {
-        pressed: false
-    },
-}
+  w: {
+    pressed: false,
+  },
+  a: {
+    pressed: false,
+  },
+  s: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+};
 
+const movables = [background, ...boundaries];
+function rectangularCollision({ rectangle1, rectangle2 }) {
+  return (
+    rectangle1.position.x + rectangle1.width >= rectangle2.position.x &&
+    rectangle1.position.x <= rectangle2.position.x + rectangle2.width &&
+    rectangle1.position.y <= rectangle2.position.y + rectangle2.height &&
+    rectangle1.position.y + rectangle1.height >= rectangle2.position.y
+  );
+}
 //tile image adjusting
-function animate(){
-    window.requestAnimationFrame(animate)
-    background.draw()
-    c.drawImage(
-        playerImage,
-        0,
-        0,
-        playerImage.width / 4,
-        playerImage.height,
-        canvas.width / 2 - playerImage.width / 4 / 2,
-        canvas.height / 2 - playerImage.height / 2,
-        playerImage.width / 4,
-        playerImage.height,
-    );
+function animate() {
+  window.requestAnimationFrame(animate);
+  background.draw();
+  boundaries.forEach((boundary) => {
+    boundary.draw();
+    if (
+      rectangularCollision({
+        rectangle1: player,
+        rectangle2: boundary,
+      })
+    ) {
+      console.log("colliding");
+    }
+  });
 
-    if(keys.w.pressed && lastKey === 'w') background.position.y += 3
-    else if(keys.a.pressed && lastKey === 'a') background.position.x += 3
-    else if(keys.d.pressed && lastKey === 'd') background.position.x -= 3
-    else if(keys.s.pressed && lastKey === 's') background.position.y -= 3
-
+  player.draw();
+  if (keys.w.pressed && lastKey === "w") {
+    movables.forEach((movable) => {
+      movable.position.y += 3;
+    });
+  } else if (keys.a.pressed && lastKey === "a") {
+    movables.forEach((movable) => {
+      movable.position.x += 3;
+    });
+  } else if (keys.d.pressed && lastKey === "d") {
+    movables.forEach((movable) => {
+      movable.position.x -= 3;
+    });
+  } else if (keys.s.pressed && lastKey === "s") {
+    movables.forEach((movable) => {
+      movable.position.y -= 3;
+    });
+  }
 }
-animate()
+animate();
 
-let lastKey = ''
+let lastKey = "";
 
 window.addEventListener("keydown", (e) => {
   switch (e.key) {
     case "w":
-        keys.w.pressed = true
-        lastKey = 'w'
-        break;
+      keys.w.pressed = true;
+      lastKey = "w";
+      break;
     case "a":
-        keys.a.pressed = true
-        lastKey = 'a'
-        break;
+      keys.a.pressed = true;
+      lastKey = "a";
+      break;
     case "s":
-        keys.s.pressed = true
-        lastKey = 's'
-        break;
+      keys.s.pressed = true;
+      lastKey = "s";
+      break;
     case "d":
-        keys.d.pressed = true
-        lastKey = 'd'
-        break;
+      keys.d.pressed = true;
+      lastKey = "d";
+      break;
   }
-  console.log(keys)
+  console.log(keys);
 });
 window.addEventListener("keyup", (e) => {
   switch (e.key) {
     case "w":
-        keys.w.pressed = false
-        break;
+      keys.w.pressed = false;
+      break;
     case "a":
-        keys.a.pressed = false
-        break;
+      keys.a.pressed = false;
+      break;
     case "s":
-        keys.s.pressed = false
-        break;
+      keys.s.pressed = false;
+      break;
     case "d":
-        keys.d.pressed = false
-        break;
+      keys.d.pressed = false;
+      break;
   }
-  console.log(keys)
+  console.log(keys);
 });
