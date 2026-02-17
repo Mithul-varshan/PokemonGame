cc3833ee33bf16bb6e8315cfa89a3734f3131916
Mithul-varshan <mithul0605@gmail.com>
2026-02-17 15:41:29 +0530

    completed battle fighting

---
+++ data/monsters.js
@@ -0,0 +1,21 @@
+const monsters = {
+    Emby: {
+        position: {
+            x: 280,
+            y: 325
+        },
+        image: {
+            src: './img/embySprite.png'
+        },
+        frames: {
+            max: 4,
+            hold: 30
+        },
+        animate: true,
+        name: 'Emby',
+        attacks: [attacks.Tackle, attacks.Fireball]
+    },
+    Draggle: {
+        
+    }
+}
