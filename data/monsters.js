32a5a309694000405de179ca111d6d1f817b3bc0
Mithul-varshan <mithul0605@gmail.com>
2026-02-17 15:54:39 +0530

    completed monster js file

--- data/monsters.js
+++ data/monsters.js
@@ -16,6 +16,20 @@ const monsters = {
         attacks: [attacks.Tackle, attacks.Fireball]
     },
     Draggle: {
-        
+        position: {
+            x: 800,
+            y: 100
+        },
+        image: {
+            src: './img/draggleSprite.png'
+        },
+        frames: {
+            max: 4,
+            hold: 30
+        },
+        animate: true,
+        isEnemy: true,
+        name: 'Draggle',
+        attacks: [attacks.Tackle, attacks.Fireball]
     }
 }
