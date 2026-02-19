74a807615cec739a46cf03a644c9c595e69bcdc5
Mithul-varshan <mithul0605@gmail.com>
2026-02-19 10:57:47 +0530

    fixed bugs

--- battleScene.js
+++ battleScene.js
@@ -1,11 +1,11 @@
 const battleBackgroundImage = new Image()
 battleBackgroundImage.src = './img/battleBackground.png'
 const battleBackground = new Sprite({
-    position: {
-        x: 0,
-        y: 0
-    },
-    image: battleBackgroundImage
+  position: {
+    x: 0,
+    y: 0
+  },
+  image: battleBackgroundImage
 })
 
 let draggle
@@ -14,113 +14,125 @@ let renderedSprites
 let battleAnimationId
 let queue
 
+let isPlayerTurn = true
+
+function setAttackButtonDisabled(disabled) {
+  document.querySelectorAll('#attacksBox button').forEach((button) => {
+    button.disabled = disabled
+  })
+}
+
 function initBattle() {
-    document.querySelector("#userInterface").style.display = 'block'
-    document.querySelector('#dialogueBox').style.display = 'none'
-    document.querySelector('#enemyHealthBar').style.width = '100%'
-    document.querySelector('#playerHealthBar').style.width = '100%'
-    document.querySelector('#attacksBox').replaceChildren()
+  document.querySelector('#userInterface').style.display = 'block'
+  document.querySelector('#dialogueBox').style.display = 'none'
+  document.querySelector('#enemyHealthBar').style.width = '100%'
+  document.querySelector('#playerHealthBar').style.width = '100%'
+  document.querySelector('#attacksBox').replaceChildren()
 
-    draggle = new Monster(monsters.Draggle)
-    emby = new Monster(monsters.Emby)
-    renderedSprites = [draggle, emby]
-    queue = []
+  draggle = new Monster(monsters.Draggle)
+  emby = new Monster(monsters.Emby)
+  renderedSprites = [draggle, emby]
+  queue = []
+  isPlayerTurn = true
+  emby.attacks.forEach((attack) => {
+    const button = document.createElement('button')
+    button.innerHTML = attack.name
+    document.querySelector('#attacksBox').append(button)
+  })
 
-    emby.attacks.forEach((attack) => {
-        const button = document.createElement('button')
-        button.innerHTML = attack.name
-        document.querySelector('#attacksBox').append(button)
-    })
+  // our event listener for our buttons (attack)
+  document.querySelectorAll('#attacksBox button').forEach((button) => {
+    button.addEventListener('click', (e) => {
+      if (!isPlayerTurn) return
+      isPlayerTurn = false
+      setAttackButtonDisabled(true)
+      const selectedAttack = attacks[e.currentTarget.innerHTML]
+      emby.attack({
+        attack: selectedAttack,
+        recipient: draggle,
+        renderedSprites
+      })
 
-    // our event listener for our buttons (attack)
-    document.querySelectorAll('button').forEach((button) => {
-        button.addEventListener('click', (e) => {
-            const selectedAttack = attacks[e.currentTarget.innerHTML]
-            emby.attack({
-                attack: selectedAttack,
-                recipient: draggle,
-                renderedSprites
-            })
+      if (draggle.health <= 0) {
+        queue.push(() => {
+          draggle.faint()
+        })
+        queue.push(() => {
+          //fade back to black
+          gsap.to('#overlappingDiv', {
+            opacity: 1,
+            onComplete: () => {
+              cancelAnimationFrame(battleAnimationId)
+              animate()
+              document.querySelector('#userInterface').style.display = 'none'
 
-            if (draggle.health <= 0){
-                queue.push(() => {
-                    draggle.faint()
-                })
-                queue.push(() => {
-                    //fade back to black
-                    gsap.to('#overlappingDiv', {
-                       opacity: 1,
-                       onComplete: () => {
-                        cancelAnimationFrame(battleAnimationId)
-                        animate()
-                        document.querySelector('#userInterface').style.display = 'none'
+              gsap.to('#overlappingDiv', {
+                opacity: 0
+              })
 
-                        gsap.to('#overlappingDiv', {
-                            opacity: 0
-                        })
-
-                        battle.initiated = false
-                        Audio.Map.play()
-                       } 
-                    })
-                })
+              battle.initiated = false
+              audio.Map.play()
             }
+          })
+        })
+      }
 
-            // draggle or enemy attacks right here
-            const randomAttack = 
-                draggle.attacks[Math.floor(Math.random() * draggle.attacks.length)]
-            
-            queue.push(() => {
-                draggle.attack({
-                    attack: randomAttack,
-                    recipient: emby,
-                    renderedSprites
+      // draggle or enemy attacks right here
+      const randomAttack =
+        draggle.attacks[Math.floor(Math.random() * draggle.attacks.length)]
+
+      queue.push(() => {
+        draggle.attack({
+          attack: randomAttack,
+          recipient: emby,
+          renderedSprites
+        })
+
+        if (emby.health <= 0) {
+          queue.push(() => {
+            emby.faint()
+          })
+
+          queue.push(() => {
+            // fade back to black
+            gsap.to('#overlappingDiv', {
+              opacity: 1,
+              onComplete: () => {
+                cancelAnimationFrame(battleAnimationId)
+                animate()
+                document.querySelector('#userInterface').style.display = 'none'
+
+                gsap.to('#overlappingDiv', {
+                  opacity: 0
                 })
-                
-                if (emby.health <= 0) {
-                    queue.push(() => {
-                        emby.faint()
-                    })
 
-                    queue.push(() => {
-                        // fade back to black
-                        gsap.to('#overlappingDiv', {
-                            opacity: 1,
-                            onComplete: () => {
-                                cancelAnimationFrame(battleAnimationId)
-                                animate()
-                                document.querySelector('#userInterface').style.display = 'none'
-
-                                gsap.to('#overlappingDiv', {
-                                    opacity: 0
-                                })
-
-                                battle.initiated = false
-                                audio.Map.play()
-                            }
-                        })
-                    })
-                }
+                battle.initiated = false
+                audio.Map.play()
+              }
             })
-        })
-
-        button.addEventListener('mouseenter', (e) => {
-            const selectedAttack = attacks[e.currentTarget.innerHTML]
-            document.querySelector('#attackType').innerHTML = selectedAttack.type
-            document.querySelector('#attackType').style.color = selectedAttack.color
-        })
+          })
+        }
+      })
     })
+
+    button.addEventListener('mouseenter', (e) => {
+      const selectedAttack = attacks[e.currentTarget.innerHTML]
+      document.querySelector('#attackType').innerHTML = selectedAttack.type
+      document.querySelector('#attackType').style.color = selectedAttack.color
+    })
+  })
+  setAttackButtonDisabled(false)
 }
 
 function animateBattle() {
-    battleAnimationId = window.requestAnimationFrame(animateBattle)
-    battleBackground.draw()
+  battleAnimationId = window.requestAnimationFrame(animateBattle)
+  battleBackground.draw()
 
-    console.log(battleAnimationId)
+  console.log(battleAnimationId)
 
-    renderedSprites.forEach((sprite) => {
-        sprite.draw()
-    })
+  renderedSprites.forEach((sprite) => {
+    sprite.draw()
+  })
 }
 
 animate()
@@ -128,11 +140,12 @@ animate()
 // animateBattle()
 
 document.querySelector('#dialogueBox').addEventListener('click', (e) => {
-    if (queue.length > 0) {
-        queue[0]()
-        queue.shift()
-    }
-    else{
-        e.currentTarget.style.display = 'none'
-    }
+  if (queue.length > 0) {
+    queue[0]()
+    queue.shift()
+  } else {
+    e.currentTarget.style.display = 'none'
+    isPlayerTurn = true
+    setAttackButtonDisabled(false)
+  }
 })
